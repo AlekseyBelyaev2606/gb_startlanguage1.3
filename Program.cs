2ca27525f64bee5bbbc1db048a3af7d7045fa178
AlekseyBelyaev2606 <partchoice44@gmail.com>
2023-01-27 15:01:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int first = int.Parse(Console.ReadLine());
+if(first%2 == 0){
+    Console.WriteLine("Число четное");
+}
+else{
+    Console.WriteLine("Число нечетное");
+}
